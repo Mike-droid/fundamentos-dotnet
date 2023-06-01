c7c8e595239be37e8a9899860fd4d710d1b55761
mike-droid <miguelreyesmoreno@hotmail.com>
2023-05-31 19:40:50 -0600

    Clase 16

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Por favor ingrese un nombre: ");
+var nombre = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su cargo: ");
+var cargo = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su edad: ");
+var edad = Console.ReadLine();
+
+Console.WriteLine($"Mi nombre es {nombre}, tengo {edad} años y soy {cargo}");
